ef02737790382d86ef1fc26b8b5fe6b9684d4fbe
Oteri Eyenike <teyenike1@gmail.com>
2020-01-27 01:24:33 +0100

    Building a post website using an jsonplaceholder

---
+++ app.js
@@ -0,0 +1,50 @@
+const POST_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+// fetch(url, {
+//   methods: '',
+//   headers: ''
+// });
+
+generatePost = () => {
+  const cardContainer = document.querySelector('.container');
+  const posts = getPosts();
+  posts.map(post => {
+    `
+      <div class="row row-cols-1 row-cols-md-2">
+          <div class="col mb-4">
+            <div class="card">
+              <img src="..." class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Post title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+    `;
+  });
+};
+
+const getPosts = url => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  fetch(url, options)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      return data;
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+};
+
+getHotelsData(POST_URL);
