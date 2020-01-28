939816e2fcfd14a1a810e218463f04fae8a8bbdc
Oteri Eyenike <teyenike1@gmail.com>
2020-01-28 13:09:11 +0100

    transferred all files to the dist folder

---
+++ dist/app.js
@@ -0,0 +1,66 @@
+const url = 'https://jsonplaceholder.typicode.com/posts/?userId=1';
+
+const cardContainer = document.querySelector('#hotels');
+
+const getPost = url => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  const post = fetch(url, options)
+    .then(res => res.json())
+    .then(data => data)
+    .catch(err => {
+      console.log(err, err.message);
+    });
+  return post;
+};
+
+const spinner = document.querySelector('.spin');
+
+const generatePost = () => {
+  cardContainer;
+  spinner.classList.add('show');
+  getPost(url)
+    .then(posts => {
+      posts.map(postArticle => {
+        cardContainer.innerHTML = `
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="./img/macbook.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${postArticle.title}</h5>
+              <p class="card-text">${postArticle.body}</p>
+            </div>
+          </div>
+      </div>
+      `;
+        spinner.classList.remove('show');
+        return cardContainer;
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+generatePost();
+
+
+
+// coder
+
+
+// const url = 'https://jsonplaceholder.typicode.com/posts/?userId=1';
+
+// fetch(url)
+//   .then(res => {
+//     return res.json();
+//   })
+//   .then(data => {
+//     const postObject = data;
+//     const postArray = Object.values(postObject);
+//     console.log(postArray);
+//   });
